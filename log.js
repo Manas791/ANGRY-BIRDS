dfdf1a1071b02c29c301fab0e4e77224d9f3498a
Manas791 <71217267+Manas791@users.noreply.github.com>
2020-11-04 15:18:47 +0530

    Add files via upload

---
+++ log.js
@@ -0,0 +1,9 @@
+class log extends Baseclass{
+    constructor(x, y, height,angle) {
+    super(x,y,20,height,angle)
+      Matter.Body.setAngle(this.body,angle)
+    this.image=loadImage("wood2.png") 
+    }
+  
+  };
+  
